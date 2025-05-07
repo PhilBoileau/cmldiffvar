d2e045458743daf5cce9ae199a029a8570e82074
Philippe Boileau <philippe.boileau@mcgill.ca>
2025-05-07 15:01:23 -0400

    refactor code and documentation

--- R/group-specific-variance-estimators.R
+++ R/group-specific-variance-estimators.R
@@ -1,7 +1,7 @@
 #' One-Step Estimator of Group-Specific Variance
 #'
-#' @description `one_step_var_estimator_fun()` estimates of the
-#'   treatment-group-specific variance using a one-step estimator.
+#' @description `one_step_var_estimator_fun()` estimates of the group-specific
+#'   variance using a one-step estimator.
 #'
 #' @param treatment_group A binary `numeric` indicating the treatment group for
 #'   which the variance is being estimated.
@@ -35,38 +35,50 @@ one_step_var_estimator_fun <- function(
   mean_est
 ) {
 
-  mean(
-    (treatment_vec == treatment_group) *
-      (outcome_vec^2 - 2 * outcome_vec * mean_est +
-        2 * cond_exp_outcome_est_vec * mean_est - cond_exp_sq_outcome_est_vec) /
-      ((treatment_group == 1) * ps_est_vec +
-        (treatment_group == 0) * (1 - ps_est_vec)) +
-    cond_exp_sq_outcome_est_vec - 2 * cond_exp_outcome_est_vec * mean_est +
-      mean_est^2
+  # compute the plug-in estimate
+  plugin_var_est <- plugin_var_estimator_fun(
+    cond_exp_outcome_est_vec,
+    cond_exp_sq_outcome_est_vec
   )
 
-}
-
-
-#' Targeted Maximum Likelihood Estimator of Group-Specific Variance
-#'
-#' @description `tml_var_estimator_fun()` estimates of the
-#'   treatment-group-specific variance using a targeted maximum likelihood
-#'   estimator.
-#'
-#' @inheritParams one_step_var_estimator_fun
-#'
-#' @returns A `numeric` estimate of the `treatment_group`-specific variance.
-#'
-#' @keywords internal
-#'
-tml_var_estimator_fun <- function(
+  # compute the eif
+  eif_vec <- efficient_influence_function_fun(
     treatment_group,
     treatment_vec,
     outcome_vec,
     ps_est_vec,
     cond_exp_outcome_est_vec,
-    cond_exp_sq_outcome_est_vec
+    cond_exp_sq_outcome_est_vec,
+    mean_est
+  )
+
+  # compute the one-step estimate
+  os_est <- mean(eif_vec) + plugin_var_est
+
+  return(os_est)
+}
+
+
+#' Targeted Maximum Likelihood Estimator of Group-Specific Variance
+#'
+#' @description `tml_var_estimator_fun()` estimates of the group-specific
+#'   variance using a targeted maximum likelihood estimator.
+#'
+#' @inheritParams one_step_var_estimator_fun
+#'
+#' @returns A `numeric` estimate of the `treatment_group`-specific variance.
+#'
+#' @inherit one_step_var_estimator_fun return
+#'
+#' @keywords internal
+#'
+tml_var_estimator_fun <- function(
+  treatment_group,
+  treatment_vec,
+  outcome_vec,
+  ps_est_vec,
+  cond_exp_outcome_est_vec,
+  cond_exp_sq_outcome_est_vec
 ) {
 
   # bound the outcome vector
@@ -102,45 +114,141 @@ tml_var_estimator_fun <- function(
     (cond_exp_sq_outcome_est_vec - min_sq_outcome_vec) /
     (max_sq_outcome_vec - min_sq_outcome_vec)
 
-  # tilt the conditional expected outcome estimator
+  # define the clever covariate
   clever_covariate <- (treatment_vec == treatment_group) /
     ((treatment_group == 1) * ps_est_vec +
        (treatment_group == 0) * (1 - ps_est_vec))
+
+  # tilt the conditional expected outcome estimator
+  # NOTE: Make sure offset terms aren't NaN
+  logit_bounded_cond_exp_outcome_est_vec <-stats::qlogis(
+    bound_away_from_0_and_1_fun(bounded_cond_exp_outcome_est_vec)
+  )
   tilted_bounded_cond_exp_outcome_fit <- suppressWarnings(
     glm(
       bounded_outcome_vec ~ -1 + clever_covariate,
       family = "binomial",
-      offset = stats::qlogis(bounded_cond_exp_outcome_est_vec)
+      offset = logit_bounded_cond_exp_outcome_est_vec
     )
   )
-  tilted_bounded_cond_exp_outcome_vec <- predict(
+  tilted_bounded_cond_exp_outcome_est_vec <- predict(
     tilted_bounded_cond_exp_outcome_fit,
     type = "response"
   )
-  tilted_cond_exp_outcome_vec <-
-    tilted_bounded_cond_exp_outcome_vec * (max_outcome_vec - min_outcome_vec) +
-      min_outcome_vec
+  tilted_cond_exp_outcome_est_vec <-
+    tilted_bounded_cond_exp_outcome_est_vec *
+      (max_outcome_vec - min_outcome_vec) + min_outcome_vec
 
   # tilt the conditional expected squared outcome estimator
-  # NOTE: add epsilon to avoid qlogis of zero error
-  tilted_bounded_sq_cond_exp_outcome_fit <- suppressWarnings(
+  # NOTE: Make sure offset terms aren't NaN
+  logit_bounded_cond_exp_sq_outcome_est_vec <-stats::qlogis(
+    bound_away_from_0_and_1_fun(bounded_cond_exp_sq_outcome_est_vec)
+  )
+  tilted_bounded_cond_exp_sq_outcome_fit <- suppressWarnings(
     glm(
       bounded_sq_outcome_vec ~ -1 + clever_covariate,
       family = "binomial",
-      offset = stats::qlogis(
-        bounded_cond_exp_sq_outcome_est_vec + .Machine$double.eps
-      )
+      offset = logit_bounded_cond_exp_sq_outcome_est_vec
     )
   )
-  tilted_bounded_cond_exp_sq_outcome_vec <- predict(
-    tilted_bounded_sq_cond_exp_outcome_fit,
+  tilted_bounded_cond_exp_sq_outcome_est_vec <- predict(
+    tilted_bounded_cond_exp_sq_outcome_fit,
     type = "response"
   )
-  tilted_cond_exp_sq_outcome_vec <-
-    tilted_bounded_cond_exp_sq_outcome_vec *
+  tilted_cond_exp_sq_outcome_est_vec <-
+    tilted_bounded_cond_exp_sq_outcome_est_vec *
       (max_sq_outcome_vec - min_sq_outcome_vec) + min_sq_outcome_vec
 
   # compute the targeted maximum likelihood estimate
-  mean(tilted_cond_exp_sq_outcome_vec) - mean(tilted_cond_exp_outcome_vec)^2
+  tml_est <- plugin_var_estimator_fun(
+    cond_exp_outcome_est_vec = tilted_cond_exp_outcome_est_vec,
+    cond_exp_sq_outcome_est_vec = tilted_cond_exp_sq_outcome_est_vec
+  )
 
+  return(tml_est)
+
+}
+
+
+#' Efficient Influence Function of Group-Specific Variance
+#'
+#' @description `efficient_influence_function_fun()` computes the efficient
+#'   influence function based on the observed data and nuisance parameter
+#'   estimates for the specified treatment group.
+#'
+#' @inheritParams one_step_var_estimator_fun
+#'
+#' @returns A `numeric` vector corresponding to the efficient influence
+#'   function.
+#'
+#' @keywords internal
+efficient_influence_function_fun <- function(
+  treatment_group,
+  treatment_vec,
+  outcome_vec,
+  ps_est_vec,
+  cond_exp_outcome_est_vec,
+  cond_exp_sq_outcome_est_vec,
+  mean_est
+) {
+
+  # compute the plug-in estimate
+  plugin_var_est <- plugin_var_estimator_fun(
+    cond_exp_outcome_est_vec,
+    cond_exp_sq_outcome_est_vec
+  )
+
+  # compute the efficient influence function
+  eif_vec <- (treatment_vec == treatment_group) *
+    (outcome_vec^2 - 2 * outcome_vec * mean_est +
+       2 * cond_exp_outcome_est_vec * mean_est - cond_exp_sq_outcome_est_vec) /
+    ((treatment_group == 1) * ps_est_vec +
+       (treatment_group == 0) * (1 - ps_est_vec)) +
+    cond_exp_sq_outcome_est_vec - 2 * cond_exp_outcome_est_vec * mean_est +
+    mean_est^2 - plugin_var_est
+
+  return(eif_vec)
+
+}
+
+
+#' Plug-In Estimator of the Group-Specific Variance
+#'
+#' @description `plugin_var_estimator_fun()` estimates of the group-specific
+#'   variance using a plug-in estimator.
+#'
+#' @inheritParams one_step_var_estimator_fun
+#'
+#' @inherit one_step_var_estimator_fun return
+#'
+#' @keywords internal
+#'
+plugin_var_estimator_fun <- function(
+  cond_exp_outcome_est_vec,
+  cond_exp_sq_outcome_est_vec
+) {
+
+  # definition of plug-in estimator
+  mean(cond_exp_sq_outcome_est_vec) - mean(cond_exp_outcome_est_vec)^2
+}
+
+
+#' Bound Vectors on Unit Interval Away from Zero and One
+#'
+#' @param unit_interval_vec A numeric interval with values between zero and one.
+#'
+#' @returns A `numeric` vector with values bounded away from zero and one by
+#'  some small value.
+#'
+#' @keywords internal
+bound_away_from_0_and_1_fun <- function(unit_interval_vec) {
+
+  # define the epsilon
+  epsilon <- .Machine$double.eps
+
+  # shift values in unit_interval_vec away from 0 and 1 by epsilon
+  unit_interval_vec[unit_interval_vec < epsilon] <- epsilon
+  unit_interval_vec[unit_interval_vec > 1 - epsilon] <- 1 - epsilon
+
+  return(unit_interval_vec)
 }
