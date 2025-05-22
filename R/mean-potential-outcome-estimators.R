be5dae5c1f35bf0fda079fdb4060ec43c0fa94f7
Philippe Boileau <philippe.boileau@mcgill.ca>
2025-05-22 15:27:55 -0400

    create one-step estiator for group-specific means

---
+++ R/mean-potential-outcome-estimators.R
@@ -0,0 +1,21 @@
+one_step_mean_estimator_fun <- function(
+    treatment_group,
+    treatment_vec,
+    outcome_vec,
+    ps_est_vec,
+    cond_exp_outcome_est_vec
+) {
+
+  # uncentered efficient influence function of the potential outcome mean
+  uncentered_eif_vec <-
+    (treatment_vec == treatment_group) *
+      (outcome_vec - cond_exp_outcome_est_vec) /
+        ((treatment_group == 1) * ps_est_vec +
+           (treatment_group == 0) * (1 - ps_est_vec)) +
+    cond_exp_outcome_est_vec
+
+  # one-step estimate
+  mean_est <- mean(uncentered_eif_vec)
+
+  return(mean_est)
+}
