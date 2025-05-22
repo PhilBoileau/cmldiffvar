78e286ebf59dc1bae8d0bf9d59846bb9b4206b27
Philippe Boileau <philippe.boileau@mcgill.ca>
2025-05-22 13:31:08 -0400

    propensity score SL estimator docs

--- R/nuisance-parameter-estimation.R
+++ R/nuisance-parameter-estimation.R
@@ -1,3 +1,23 @@
+#' SuperLearned Propensity Score
+#'
+#' `estimate_propensity_score_fun()` estimates the propensity score using a
+#' cross-validated SuperLearner-based estimator implemented in the
+#' [SuperLearner::CV.SuperLearner()] function.
+#'
+#' @param clean_tbl A pre-processed [tibble] that is ready for nuisance
+#'   parameter estimation.
+#' @param confounder_var_names A `character` vector providing the columns names
+#'   of the treatment--outcome confounders stored in `clean_tbl`.
+#' @param treatment_var_name A `character` providing the columns name of the
+#'   treatment assignment indicator stored in `clean_tbl`.
+#' @param propensity_score_library A `character` vector of candidate learners
+#'   used by the SuperLearner estimator.
+#' @param num_folds A `numeric` indicating the number of folds to use in
+#'   cross-validated SuperLearner estimator.
+#'
+#' @returns A [SuperLearner::CV.SuperLearner] class object containing the
+#' estimated propensity score.
+#'
 estimate_propensity_score_fun <- function(
   clean_tbl,
   confounder_var_names,
