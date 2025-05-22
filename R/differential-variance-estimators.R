fef98a35d0588f637a5bd5e872fa546912380c55
Philippe Boileau <philippe.boileau@mcgill.ca>
2025-05-22 14:44:15 -0400

    countefactual dataset generator

---
+++ R/differential-variance-estimators.R
@@ -0,0 +1,53 @@
+generate_counterfactural_tbl_fun <- function(
+  clean_tbl,
+  treatment_group,
+  confounder_var_names,
+  treatment_var_name,
+  propensity_score_sl_fit,
+  cond_exp_outcome_sl_fit,
+  cond_exp_sq_outcome_sl_fit
+) {
+
+  # create counterfactual dataset: all units assigned to treatment_group
+  clean_counterfactual_tbl <- clean_tbl |>
+    dplyr::mutate(!!treatment_var_name := treatment_group)
+
+  # predict nuisance parameters under counterfactual scenarios
+  pred_propensity_score <- predict(
+    propensity_score_sl_fit,
+    newdata = clean_counterfactual_tbl |>
+      dplyr::select(dplyr::all_of(confounder_var_names)),
+    onlySL = TRUE
+  )$pred
+  pred_cond_exp_outcome <- predict(
+    cond_exp_outcome_sl_fit,
+    newdata = clean_counterfactual_tbl |>
+      dplyr::select(
+        dplyr::all_of(
+          c(confounder_var_names, treatment_var_name)
+        )
+      ),
+    onlySL = TRUE
+  )$pred
+  pred_cond_exp_sq_outcome <- predict(
+    cond_exp_sq_outcome_sl_fit,
+    newdata = clean_counterfactual_tbl |>
+      dplyr::select(
+        dplyr::all_of(
+          c(confounder_var_names, treatment_var_name)
+        )
+      ),
+    onlySL = TRUE
+  )$pred
+
+  # add predictions to counterfactual dataset
+  clean_counterfactual_tbl <- clean_counterfactual_tbl |>
+    dplyr::mutate(
+      pred_propensity_score = pred_propensity_score,
+      pred_cond_exp_outcome = pred_cond_exp_outcome,
+      pred_cond_exp_sq_outcome = pred_cond_exp_sq_outcome
+    )
+
+  return(clean_counterfactual_tbl)
+
+}
---
+++ tests/testthat/test-differential-variance-estimator.R
@@ -0,0 +1,82 @@
+test_that("counterfactual table generator produces counterfactual tables", {
+
+  # load required libraries
+  library(dplyr)
+  library(SuperLearner)
+
+  # set seed for reproducibility
+  set.seed(234566)
+
+  # grab a sample of the population
+  sample_tbl <- slice_sample(toy_population_tbl, n = 100)
+
+  # fit nuisance parameter estimators
+  ps_sl_fit <- estimate_propensity_score_fun(
+    sample_tbl,
+    confounder_var_names = "confounder",
+    treatment_var_name = "treatment",
+    propensity_score_library = c("SL.glm", "SL.mean"),
+    num_folds = 5
+  )
+  cond_exp_outcome_fit <- estimate_cond_exp_outcome_fun(
+    sample_tbl,
+    confounder_var_names = "confounder",
+    treatment_var_name = "treatment",
+    outcome_var_name = "outcome",
+    cond_exp_outcome_library = c("SL.glm", "SL.mean"),
+    num_folds = 5
+  )
+  cond_exp_sq_outcome_fit <- estimate_cond_exp_sq_outcome_fun(
+    sample_tbl,
+    confounder_var_names = "confounder",
+    treatment_var_name = "treatment",
+    outcome_var_name = "outcome",
+    cond_exp_sq_outcome_library = c("SL.glm", "SL.earth"),
+    num_folds = 5
+  )
+
+  # construct the counterfactual dataset under the treatment group
+  sample_treatment_tbl <- generate_counterfactural_tbl_fun(
+    sample_tbl,
+    treatment_group = 1,
+    confounder_var_names = "confounder",
+    treatment_var_name = "treatment",
+    ps_sl_fit,
+    cond_exp_outcome_fit,
+    cond_exp_sq_outcome_fit
+  )
+
+  # all treatment indicators are set to treatment (1)
+  expect(sum(sample_treatment_tbl$treatment), nrow(sample_treatment_tbl))
+
+  # predictions from nuisance parameters match manula calculations
+  expect_equal(
+    as.numeric(sample_treatment_tbl$pred_propensity_score),
+    as.numeric(
+      predict(ps_sl_fit, newdata = sample_tbl |> select(confounder))$pred
+    )
+  )
+  expect_equal(
+    as.numeric(sample_treatment_tbl$pred_cond_exp_outcome),
+    as.numeric(
+      predict(
+        cond_exp_outcome_fit,
+        newdata = sample_tbl |>
+          mutate(treatment = 1) |>
+          select(treatment, confounder)
+      )$pred
+    )
+  )
+  expect_equal(
+    as.numeric(sample_treatment_tbl$pred_cond_exp_sq_outcome),
+    as.numeric(
+      predict(
+        cond_exp_sq_outcome_fit,
+        newdata = sample_tbl |>
+          mutate(treatment = 1) |>
+          select(treatment, confounder)
+      )$pred
+    )
+  )
+
+})
