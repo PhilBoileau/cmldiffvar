cc8ca88b49c558344d8602164b7667e3b973c045
Philippe Boileau <philippe.boileau@mcgill.ca>
2025-06-05 08:40:10 -0400

    simple integration tests for serial parallel cmldiffvar()

---
+++ tests/testthat/test-cmldiffvar-integration.R
@@ -0,0 +1,342 @@
+test_that(
+  "serial cmldiffvarr estimates diff vars without warnings or errors",
+{
+
+  set.seed(2352314)
+
+  # sample tibble
+  sample_tbl <- slice_sample(toy_population_tbl, n = 500)
+
+  # TMLE for absolute diff var
+  expect_silent(
+    sample_tbl |>
+      cmldiffvar(
+        estimand_type = "absolute",
+        estimator_type = "tmle",
+        confidence_level = 0.95,
+        confounder_var_names = "confounder",
+        treatment_var_name = "treatment",
+        outcome_var_name = "outcome",
+        propensity_score_library = c("SL.mean", "SL.glm", "SL.earth"),
+        cond_exp_outcome_library = c("SL.mean", "SL.glm", "SL.earth"),
+        cond_exp_sq_outcome_library = c("SL.mean", "SL.glm", "SL.earth"),
+        num_nuisance_sl_folds = 5,
+        cross_fit = FALSE,
+        num_cross_fit_folds = 5,
+        parallel = FALSE
+      )
+    )
+
+  # TMLE for relative diff var
+  expect_silent(
+    sample_tbl |>
+      cmldiffvar(
+        estimand_type = "relative",
+        estimator_type = "tmle",
+        confidence_level = 0.95,
+        confounder_var_names = "confounder",
+        treatment_var_name = "treatment",
+        outcome_var_name = "outcome",
+        propensity_score_library = c("SL.mean", "SL.glm", "SL.earth"),
+        cond_exp_outcome_library = c("SL.mean", "SL.glm", "SL.earth"),
+        cond_exp_sq_outcome_library = c("SL.mean", "SL.glm", "SL.earth"),
+        num_nuisance_sl_folds = 5,
+        cross_fit = FALSE,
+        num_cross_fit_folds = 5,
+        parallel = FALSE
+      )
+  )
+
+  # cross-fitted TMLE for absolute diff var
+  expect_silent(
+    sample_tbl |>
+      cmldiffvar(
+        estimand_type = "absolute",
+        estimator_type = "tmle",
+        confidence_level = 0.95,
+        confounder_var_names = "confounder",
+        treatment_var_name = "treatment",
+        outcome_var_name = "outcome",
+        propensity_score_library = c("SL.mean", "SL.glm", "SL.earth"),
+        cond_exp_outcome_library = c("SL.mean", "SL.glm", "SL.earth"),
+        cond_exp_sq_outcome_library = c("SL.mean", "SL.glm", "SL.earth"),
+        num_nuisance_sl_folds = 5,
+        cross_fit = TRUE,
+        num_cross_fit_folds = 5,
+        parallel = FALSE
+      )
+  )
+
+  # cross-fitted TMLE for relative diff var
+  expect_silent(
+    sample_tbl |>
+      cmldiffvar(
+        estimand_type = "relative",
+        estimator_type = "tmle",
+        confidence_level = 0.95,
+        confounder_var_names = "confounder",
+        treatment_var_name = "treatment",
+        outcome_var_name = "outcome",
+        propensity_score_library = c("SL.mean", "SL.glm", "SL.earth"),
+        cond_exp_outcome_library = c("SL.mean", "SL.glm", "SL.earth"),
+        cond_exp_sq_outcome_library = c("SL.mean", "SL.glm", "SL.earth"),
+        num_nuisance_sl_folds = 5,
+        cross_fit = TRUE,
+        num_cross_fit_folds = 5,
+        parallel = FALSE
+      )
+  )
+
+  # one-step for absolute diff var
+  expect_silent(
+    sample_tbl |>
+      cmldiffvar(
+        estimand_type = "absolute",
+        estimator_type = "one-step",
+        confidence_level = 0.95,
+        confounder_var_names = "confounder",
+        treatment_var_name = "treatment",
+        outcome_var_name = "outcome",
+        propensity_score_library = c("SL.mean", "SL.glm", "SL.earth"),
+        cond_exp_outcome_library = c("SL.mean", "SL.glm", "SL.earth"),
+        cond_exp_sq_outcome_library = c("SL.mean", "SL.glm", "SL.earth"),
+        num_nuisance_sl_folds = 5,
+        cross_fit = FALSE,
+        num_cross_fit_folds = 5,
+        parallel = FALSE
+      )
+  )
+
+  # one-step for relative diff var
+  expect_silent(
+    sample_tbl |>
+      cmldiffvar(
+        estimand_type = "relative",
+        estimator_type = "one-step",
+        confidence_level = 0.95,
+        confounder_var_names = "confounder",
+        treatment_var_name = "treatment",
+        outcome_var_name = "outcome",
+        propensity_score_library = c("SL.mean", "SL.glm", "SL.earth"),
+        cond_exp_outcome_library = c("SL.mean", "SL.glm", "SL.earth"),
+        cond_exp_sq_outcome_library = c("SL.mean", "SL.glm", "SL.earth"),
+        num_nuisance_sl_folds = 5,
+        cross_fit = FALSE,
+        num_cross_fit_folds = 5,
+        parallel = FALSE
+      )
+  )
+
+  # cross-fitted one-step for absolute diff var
+  expect_silent(
+    sample_tbl |>
+      cmldiffvar(
+        estimand_type = "absolute",
+        estimator_type = "one-step",
+        confidence_level = 0.95,
+        confounder_var_names = "confounder",
+        treatment_var_name = "treatment",
+        outcome_var_name = "outcome",
+        propensity_score_library = c("SL.mean", "SL.glm", "SL.earth"),
+        cond_exp_outcome_library = c("SL.mean", "SL.glm", "SL.earth"),
+        cond_exp_sq_outcome_library = c("SL.mean", "SL.glm", "SL.earth"),
+        num_nuisance_sl_folds = 5,
+        cross_fit = TRUE,
+        num_cross_fit_folds = 5,
+        parallel = FALSE
+      )
+  )
+
+  # cross-fitted one-step for relative diff var
+  expect_silent(
+    sample_tbl |>
+      cmldiffvar(
+        estimand_type = "relative",
+        estimator_type = "one-step",
+        confidence_level = 0.95,
+        confounder_var_names = "confounder",
+        treatment_var_name = "treatment",
+        outcome_var_name = "outcome",
+        propensity_score_library = c("SL.mean", "SL.glm", "SL.earth"),
+        cond_exp_outcome_library = c("SL.mean", "SL.glm", "SL.earth"),
+        cond_exp_sq_outcome_library = c("SL.mean", "SL.glm", "SL.earth"),
+        num_nuisance_sl_folds = 5,
+        cross_fit = TRUE,
+        num_cross_fit_folds = 5,
+        parallel = FALSE
+      )
+  )
+
+})
+
+test_that("parallelized cmldiffvarr works",{
+
+  library(future)
+
+  plan(sequential)
+
+  set.seed(2352314)
+
+  # sample tibble
+  sample_tbl <- slice_sample(toy_population_tbl, n = 500)
+
+  # TMLE for absolute diff var
+  expect_silent(
+    sample_tbl |>
+      cmldiffvar(
+        estimand_type = "absolute",
+        estimator_type = "tmle",
+        confidence_level = 0.95,
+        confounder_var_names = "confounder",
+        treatment_var_name = "treatment",
+        outcome_var_name = "outcome",
+        propensity_score_library = c("SL.mean", "SL.glm", "SL.earth"),
+        cond_exp_outcome_library = c("SL.mean", "SL.glm", "SL.earth"),
+        cond_exp_sq_outcome_library = c("SL.mean", "SL.glm", "SL.earth"),
+        num_nuisance_sl_folds = 5,
+        cross_fit = FALSE,
+        num_cross_fit_folds = 5,
+        parallel = TRUE
+      )
+  )
+
+  # TMLE for relative diff var
+  expect_silent(
+    sample_tbl |>
+      cmldiffvar(
+        estimand_type = "relative",
+        estimator_type = "tmle",
+        confidence_level = 0.95,
+        confounder_var_names = "confounder",
+        treatment_var_name = "treatment",
+        outcome_var_name = "outcome",
+        propensity_score_library = c("SL.mean", "SL.glm", "SL.earth"),
+        cond_exp_outcome_library = c("SL.mean", "SL.glm", "SL.earth"),
+        cond_exp_sq_outcome_library = c("SL.mean", "SL.glm", "SL.earth"),
+        num_nuisance_sl_folds = 5,
+        cross_fit = FALSE,
+        num_cross_fit_folds = 5,
+        parallel = TRUE
+      )
+  )
+
+  # cross-fitted TMLE for absolute diff var
+  expect_silent(
+    sample_tbl |>
+      cmldiffvar(
+        estimand_type = "absolute",
+        estimator_type = "tmle",
+        confidence_level = 0.95,
+        confounder_var_names = "confounder",
+        treatment_var_name = "treatment",
+        outcome_var_name = "outcome",
+        propensity_score_library = c("SL.mean", "SL.glm", "SL.earth"),
+        cond_exp_outcome_library = c("SL.mean", "SL.glm", "SL.earth"),
+        cond_exp_sq_outcome_library = c("SL.mean", "SL.glm", "SL.earth"),
+        num_nuisance_sl_folds = 5,
+        cross_fit = TRUE,
+        num_cross_fit_folds = 5,
+        parallel = TRUE
+      )
+  )
+
+  # cross-fitted TMLE for relative diff var
+  expect_silent(
+    sample_tbl |>
+      cmldiffvar(
+        estimand_type = "relative",
+        estimator_type = "tmle",
+        confidence_level = 0.95,
+        confounder_var_names = "confounder",
+        treatment_var_name = "treatment",
+        outcome_var_name = "outcome",
+        propensity_score_library = c("SL.mean", "SL.glm", "SL.earth"),
+        cond_exp_outcome_library = c("SL.mean", "SL.glm", "SL.earth"),
+        cond_exp_sq_outcome_library = c("SL.mean", "SL.glm", "SL.earth"),
+        num_nuisance_sl_folds = 5,
+        cross_fit = TRUE,
+        num_cross_fit_folds = 5,
+        parallel = TRUE
+      )
+  )
+
+  # one-step for absolute diff var
+  expect_silent(
+    sample_tbl |>
+      cmldiffvar(
+        estimand_type = "absolute",
+        estimator_type = "one-step",
+        confidence_level = 0.95,
+        confounder_var_names = "confounder",
+        treatment_var_name = "treatment",
+        outcome_var_name = "outcome",
+        propensity_score_library = c("SL.mean", "SL.glm", "SL.earth"),
+        cond_exp_outcome_library = c("SL.mean", "SL.glm", "SL.earth"),
+        cond_exp_sq_outcome_library = c("SL.mean", "SL.glm", "SL.earth"),
+        num_nuisance_sl_folds = 5,
+        cross_fit = FALSE,
+        num_cross_fit_folds = 5,
+        parallel = TRUE
+      )
+  )
+
+  # one-step for relative diff var
+  expect_silent(
+    sample_tbl |>
+      cmldiffvar(
+        estimand_type = "relative",
+        estimator_type = "one-step",
+        confidence_level = 0.95,
+        confounder_var_names = "confounder",
+        treatment_var_name = "treatment",
+        outcome_var_name = "outcome",
+        propensity_score_library = c("SL.mean", "SL.glm", "SL.earth"),
+        cond_exp_outcome_library = c("SL.mean", "SL.glm", "SL.earth"),
+        cond_exp_sq_outcome_library = c("SL.mean", "SL.glm", "SL.earth"),
+        num_nuisance_sl_folds = 5,
+        cross_fit = FALSE,
+        num_cross_fit_folds = 5,
+        parallel = TRUE
+      )
+  )
+
+  # cross-fitted one-step for absolute diff var
+  expect_silent(
+    sample_tbl |>
+      cmldiffvar(
+        estimand_type = "absolute",
+        estimator_type = "one-step",
+        confidence_level = 0.95,
+        confounder_var_names = "confounder",
+        treatment_var_name = "treatment",
+        outcome_var_name = "outcome",
+        propensity_score_library = c("SL.mean", "SL.glm", "SL.earth"),
+        cond_exp_outcome_library = c("SL.mean", "SL.glm", "SL.earth"),
+        cond_exp_sq_outcome_library = c("SL.mean", "SL.glm", "SL.earth"),
+        num_nuisance_sl_folds = 5,
+        cross_fit = TRUE,
+        num_cross_fit_folds = 5,
+        parallel = TRUE
+      )
+  )
+
+  # cross-fitted one-step for relative diff var
+  expect_silent(
+    sample_tbl |>
+      cmldiffvar(
+        estimand_type = "relative",
+        estimator_type = "one-step",
+        confidence_level = 0.95,
+        confounder_var_names = "confounder",
+        treatment_var_name = "treatment",
+        outcome_var_name = "outcome",
+        propensity_score_library = c("SL.mean", "SL.glm", "SL.earth"),
+        cond_exp_outcome_library = c("SL.mean", "SL.glm", "SL.earth"),
+        cond_exp_sq_outcome_library = c("SL.mean", "SL.glm", "SL.earth"),
+        num_nuisance_sl_folds = 5,
+        cross_fit = TRUE,
+        num_cross_fit_folds = 5,
+        parallel = TRUE
+      )
+  )
+})
