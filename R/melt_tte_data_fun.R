d2befbc075912e6255e2c12478f19ad54d294479
Philippe Boileau <philippe.boileau@mcgill.ca>
2026-05-20 17:08:21 -0400

    include indicator for rows used in nuisance estimation

--- R/melt_tte_data_fun.R
+++ R/melt_tte_data_fun.R
@@ -39,6 +39,13 @@ melt_tte_data_fun <- function(
       )
     )
 
+  # get the unique times in the dataset up to the cutoff time
+  unique_times <- wide_data_tbl |>
+    dplyr::filter(!!rlang::sym(outcome_var_name) <= time_cutoff) |>
+    dplyr::pull(outcome_var_name) |>
+    unique() |>
+    sort()
+
   # lengthen each observation
   obs_ls <- lapply(
     seq_len(nrow(wide_data_tbl)),
@@ -48,31 +55,36 @@ melt_tte_data_fun <- function(
       long_obs_tbl <- wide_data_tbl |>
         dplyr::slice(rep(obs_idx, times = time_cutoff))
 
-      # label each entry by the time, create the longitudinal indicators,
-      # and add an id
+      # label each entry by the time, create the longitudinal indicators, add an
+      # id, and indicate which times have an event associated with them for use
+      # in nuisance estimation
       long_obs_tbl <- long_obs_tbl |>
         dplyr::mutate(
-          long_time = seq_len(time_cutoff),
-          R = dplyr::if_else(long_time == .data[[outcome_var_name]] &
+          cmldiffvar_long_time = seq_len(time_cutoff),
+          R = dplyr::if_else(cmldiffvar_long_time == .data[[outcome_var_name]] &
                                .data[[censoring_var_name]] == 1, 1, 0),
-          L = dplyr::if_else(long_time == .data[[outcome_var_name]] &
+          L = dplyr::if_else(cmldiffvar_long_time == .data[[outcome_var_name]] &
                                .data[[censoring_var_name]] == 0, 1, 0),
-          I = dplyr::if_else(long_time < .data[[outcome_var_name]], 1, 0),
+          I = dplyr::if_else(cmldiffvar_long_time < .data[[outcome_var_name]],
+                             1, 0),
           J = dplyr::case_when(
             I == 1 ~ 1,
-            long_time == .data[[outcome_var_name]] &
+            cmldiffvar_long_time == .data[[outcome_var_name]] &
               .data[[censoring_var_name]] == 1 ~ 1,
             .default = 0
           ),
-          cmldiffvar_id = obs_idx
+          cmldiffvar_id = obs_idx,
+          cmldiffvar_for_nuisance_estimation = if_else(
+            cmldiffvar_long_time %in% unique_times, 1, 0)
         )
 
       # retain only the necessary variables
       long_obs_tbl |>
         dplyr::select(
           dplyr::all_of(
-            c("cmldiffvar_id", baseline_var_names, treatment_var_name, "R", "L",
-              "I", "J")
+            c("cmldiffvar_id", "cmldiffvar_long_time", baseline_var_names,
+              treatment_var_name, "R", "L", "I", "J",
+              "cmldiffvar_for_nuisance_estimation")
           )
         )
     }
