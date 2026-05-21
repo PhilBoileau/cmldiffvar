8939a0004a9175637e44625884d2e99f89f88002
Philippe Boileau <philippe.boileau@mcgill.ca>
2026-05-21 15:29:49 -0400

    add HTE AFT model DGP

--- tests/testthat/helper-tte-dgp.R
+++ tests/testthat/helper-tte-dgp.R
@@ -31,7 +31,7 @@ generate_test_data <- function(
       }
     } else {
       cond_surv_hazard <- function(time, exposure, w) {
-        (time < max_time) / (75 + exp(2 * (exposure + w + w * exposure))) +
+        (time < max_time) / (75 + exp(1 + 2 * w + 3 * exposure)) +
           (time == max_time)
       }
     }
@@ -83,13 +83,15 @@ generate_test_data <- function(
 
     # otherwise use a simple AFT model
     eps_0 <- rnorm(n_obs)
+    eps_1 <- rnorm(n_obs)
     censor_time_0 <- floor(rexp(n_obs, rate = 0.01))
     censor_time_1 <- floor(rexp(n_obs, rate = 0.01))
     if (null_marginal) {
-      failure_time_1 <- failure_time_0 <- floor(exp(3.5 - 0.4 * w + eps_0))
+      failure_time_0 <- floor(exp(3.5 - 0.4 * w + eps_0))
+      failure_time_1 <- floor(failure_time_0 * 1.1)
     } else {
       failure_time_0 <- floor(exp(3.5 - 0.4 * w + eps_0))
-      failure_time_1 <- failure_time_0 * 1.1
+      failure_time_1 <- floor(exp(3.5 + 0.4 * w + 0.25 * a + eps_1))
     }
 
   }
@@ -139,6 +141,8 @@ generate_test_data <- function(
 }
 
 # Hazard DGP ----
+
+## Homogeneous additive model with null treatment effect ----
 
 # approximate relevant parameter values at time_cutoff = 50
 # set.seed(510)
@@ -210,12 +214,94 @@ generate_test_data <- function(
 # 6  216.  279.              1.29            5
 
 
+
+## Heterogeneous additive model with null marginal RMST difference ----
+
+# approximate relevant parameter values at time_cutoff = 50
+# set.seed(948)
+# pop_sim_tbl <- generate_test_data(n_obs = 10000, null_marginal = FALSE)
+# time_cutoff <- 100
+# pop_params_tbl <- pop_sim_tbl |>
+#   mutate(
+#     trunc_potential_time_0 = if_else(
+#       potential_time_0 > time_cutoff, time_cutoff, potential_time_0
+#     ),
+#     trunc_potential_time_1 = if_else(
+#       potential_time_1 > time_cutoff, time_cutoff, potential_time_1
+#     )
+#   ) |>
+#   # group_by(w) |>
+#   summarise(
+#     rmst_0 = mean(trunc_potential_time_0),
+#     rmst_1 = mean(trunc_potential_time_1),
+#     var_0 = var(trunc_potential_time_0),
+#     var_1 = var(trunc_potential_time_1),
+#     rmst_diff = rmst_1 - rmst_0,
+#     diff_var_ratio = var_1 / var_0
+#   )
+
+# recorded values
+# $ rmst_0         <dbl> 59.0448
+# $ rmst_1         <dbl> 80.0494
+# $ var_0          <dbl> 1285.684
+# $ var_1          <dbl> 995.0123
+# $ rmst_diff      <dbl> 21.0046
+# $ diff_var_ratio <dbl> 0.7739167
+
+# compute the ratios of truncated survival times
+# add_diff_var_ratio_tbl <- lapply(
+#   seq(from = 15, to = 30),
+#   function(trunc_value) {
+#
+#     # compute the additive diff var ratio under the homogeneous additive
+#     # treatment effect assumption
+#     add_diff_var_ratio_tbl <- pop_sim_tbl |>
+#       mutate(
+#         trunc_trunc_potential_time_0 = if_else(
+#           potential_time_0 > (time_cutoff - trunc_value),
+#           (time_cutoff - trunc_value), potential_time_0
+#         ),
+#         trunc_potential_time_1 = if_else(
+#           potential_time_1 > time_cutoff, time_cutoff, potential_time_1
+#         )
+#       ) |>
+#       summarise(
+#         var_0 = var(trunc_trunc_potential_time_0),
+#         var_1 = var(trunc_potential_time_1),
+#         add_diff_var_ratio = var_1 / var_0
+#       ) |>
+#       mutate(trunc_value = trunc_value)
+#
+#     return(add_diff_var_ratio_tbl)
+#   }
+# ) |>
+#   bind_rows()
+
+# diff_var_ratio_tbl output:
+#    <dbl> <dbl>              <dbl>       <int>
+#  1  924.  995.               1.08          15
+#  2  901.  995.               1.10          16
+#  3  878.  995.               1.13          17
+#  4  856.  995.               1.16          18
+#  5  834.  995.               1.19          19
+#  6  812.  995.               1.23          20
+#  7  790.  995.               1.26          21
+#  8  768.  995.               1.29          22
+#  9  747.  995.               1.33          23
+# 10  726.  995.               1.37          24
+# 11  705.  995.               1.41          25
+# 12  685.  995.               1.45          26
+# 13  664.  995.               1.50          27
+# 14  644.  995.               1.54          28
+# 15  624.  995.               1.59          29
+# 16  605.  995.               1.65          30
+
 # AFT DGP ----
 
-## Null marginal model ----
+## Homogeneous non-null treatment effect model ----
 
-# # approximate relevant parameter values at time_cutoff = 50
-# set.seed(486415)
+# approximate relevant parameter values at time_cutoff = 100
+# set.seed(19984)
 # pop_sim_tbl <- generate_test_data(
 #   n_obs = 100000, null_marginal = TRUE, hazard_model = FALSE
 # )
@@ -237,18 +323,18 @@ generate_test_data <- function(
 #     rmst_ratio = rmst_1 / rmst_0,
 #     diff_var_ratio = var_1 / var_0
 #   )
-#
-# # recorded values
-# # $ rmst_0         <dbl> 47.24593
-# # $ rmst_1         <dbl> 47.05074
-# # $ var_0          <dbl> 1026.412
-# # $ var_1          <dbl> 1030.285
-# # $ rmst_ratio     <dbl> 0.9958686
-# # $ diff_var_ratio <dbl> 1.003774
-#
-# # compute the ratios of truncated survival times
+
+# recorded values
+# $ rmst_0         <dbl> 36.92097
+# $ rmst_1         <dbl> 39.53811
+# $ var_0          <dbl> 916.7922
+# $ var_1          <dbl> 970.0174
+# $ rmst_ratio     <dbl> 1.070885
+# $ diff_var_ratio <dbl> 1.058056
+
+# compute the ratios of truncated survival times
 # mult_diff_var_ratio_tbl <- lapply(
-#   seq(from = 0.90, to = 1.0, by = 0.01),
+#   seq(from = 0.85, to = 1.00, by = 0.01),
 #   function(trunc_value) {
 #
 #     # compute the multiplicative diff var ratio under the homogeneous
@@ -266,7 +352,8 @@ generate_test_data <- function(
 #       summarise(
 #         var_0 = var(trunc_trunc_potential_time_0),
 #         var_1 = var(trunc_potential_time_1),
-#         mult_diff_var_ratio = var_1 / var_0
+#         mult_diff_var_ratio = var_1 / var_0,
+#         standardized_diff_var = mult_diff_var_ratio - (1 / trunc_value)^2
 #       ) |>
 #       mutate(trunc_value = trunc_value)
 #
@@ -274,29 +361,36 @@ generate_test_data <- function(
 #   }
 # ) |>
 #   bind_rows()
-#
-# # mult_diff_var_ratio_tbl output
-# #    var_0 var_1 mult_diff_var_ratio trunc_value
-# #    <dbl> <dbl>               <dbl>       <dbl>
-# #  1  841. 1030.                1.23        0.9
-# #  2  859. 1030.                1.20        0.91
-# #  3  877. 1030.                1.17        0.92
-# #  4  896. 1030.                1.15        0.93
-# #  5  914. 1030.                1.13        0.94
-# #  6  933. 1030.                1.10        0.95
-# #  7  951. 1030.                1.08        0.96
-# #  8  970. 1030.                1.06        0.97
-# #  9  989. 1030.                1.04        0.98
-# # 10 1007. 1030.                1.02        0.99
-# # 11 1026. 1030.                1.00        1
 
+# mult_diff_var_ratio_tbl output
+#    var_0 var_1 mult_diff_var_ratio standardized_diff_var trunc_value
+#    <dbl> <dbl>               <dbl>                 <dbl>       <dbl>
+#  1  726.  970.                1.34             -0.0481          0.85
+#  2  739.  970.                1.31             -0.0393          0.86
+#  3  752.  970.                1.29             -0.0308          0.87
+#  4  765.  970.                1.27             -0.0226          0.88
+#  5  777.  970.                1.25             -0.0146          0.89
+#  6  790.  970.                1.23             -0.00681         0.9
+#  7  803.  970.                1.21              0.000655        0.91
+#  8  816.  970.                1.19              0.00787         0.92
+#  9  828.  970.                1.17              0.0149          0.93
+# 10  841.  970.                1.15              0.0216          0.94
+# 11  854.  970.                1.14              0.0281          0.95
+# 12  866.  970.                1.12              0.0344          0.96
+# 13  879.  970.                1.10              0.0406          0.97
+# 14  892.  970.                1.09              0.0466          0.98
+# 15  904.  970.                1.07              0.0524          0.99
+# 16  917.  970.                1.06              0.0581          1
 
-## Heterogenous model ----
+# as expected, the standardized differential variance is approximately equal to
+# 0 when the truncation value is near (1/1.1), indicating a homogeneous but
+# non-null effect
 
+## Heterogeneous treatment effect model ----
 # approximate relevant parameter values at time_cutoff = 100
-set.seed(19984)
+set.seed(824563)
 pop_sim_tbl <- generate_test_data(
-  n_obs = 100000, null_marginal = TRUE, hazard_model = FALSE
+  n_obs = 100000, null_marginal = FALSE, hazard_model = FALSE
 )
 time_cutoff <- 100
 pop_params_tbl <- pop_sim_tbl |>
@@ -318,16 +412,16 @@ pop_params_tbl <- pop_sim_tbl |>
   )
 
 # recorded values
-# $ rmst_0         <dbl> 36.92097
-# $ rmst_1         <dbl> 36.92097
-# $ var_0          <dbl> 916.7922
-# $ var_1          <dbl> 916.7922
-# $ rmst_ratio     <dbl> 1
-# $ diff_var_ratio <dbl> 1
+# $ rmst_0         <dbl> 37.12697
+# $ rmst_1         <dbl> 52.82716
+# $ var_0          <dbl> 917.5128
+# $ var_1          <dbl> 1135.992
+# $ rmst_ratio     <dbl> 1.422878
+# $ diff_var_ratio <dbl> 1.238121
 
 # compute the ratios of truncated survival times
 mult_diff_var_ratio_tbl <- lapply(
-  seq(from = 0.90, to = 1.00, by = 0.01),
+  seq(from = 0.6, to = 0.75, by = 0.01),
   function(trunc_value) {
 
     # compute the multiplicative diff var ratio under the homogeneous
@@ -355,17 +449,22 @@ mult_diff_var_ratio_tbl <- lapply(
 ) |>
   bind_rows()
 
-# mult_diff_var_ratio_tbl output
+# mult_diff_var_ratio_tbl output:
 #    var_0 var_1 mult_diff_var_ratio standardized_diff_var trunc_value
 #    <dbl> <dbl>               <dbl>                 <dbl>       <dbl>
-#  1  790.  917.                1.16              -0.0742         0.9
-#  2  803.  917.                1.14              -0.0656         0.91
-#  3  816.  917.                1.12              -0.0574         0.92
-#  4  828.  917.                1.11              -0.0494         0.93
-#  5  841.  917.                1.09              -0.0417         0.94
-#  6  854.  917.                1.07              -0.0342         0.95
-#  7  866.  917.                1.06              -0.0270         0.96
-#  8  879.  917.                1.04              -0.0199         0.97
-#  9  892.  917.                1.03              -0.0131         0.98
-# 10  904.  917.                1.01              -0.00646        0.99
-# 11  917.  917.                1                  0              1
+#  1  406. 1136.                2.80                0.0222        0.6
+#  2  418. 1136.                2.72                0.0290        0.61
+#  3  431. 1136.                2.64                0.0361        0.62
+#  4  443. 1136.                2.56                0.0434        0.63
+#  5  456. 1136.                2.49                0.0506        0.64
+#  6  469. 1136.                2.42                0.0578        0.65
+#  7  481. 1136.                2.36                0.0651        0.66
+#  8  494. 1136.                2.30                0.0723        0.67
+#  9  507. 1136.                2.24                0.0794        0.68
+# 10  519. 1136.                2.19                0.0865        0.69
+# 11  532. 1136.                2.13                0.0935        0.7
+# 12  545. 1136.                2.08                0.100         0.71
+# 13  558. 1136.                2.04                0.107         0.72
+# 14  571. 1136.                1.99                0.113         0.73
+# 15  584. 1136.                1.95                0.120         0.74
+# 16  597. 1136.                1.90                0.126         0.75
