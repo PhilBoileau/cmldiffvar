bdeb30cd6be8bbae8dd5112475ee74583064d8cd
Philippe Boileau <philippe.boileau@mcgill.ca>
2026-05-21 09:59:16 -0400

    compute population parameters under the null model

--- tests/testthat/helper-tte-dgp.R
+++ tests/testthat/helper-tte-dgp.R
@@ -112,8 +112,72 @@ generate_test_data <- function(
     prop_score = prop_score,
     a = a,
     time = time,
-    censoring = censoring
+    censoring = censoring,
+    potential_time_0 = failure_time_0,
+    potential_time_1 = failure_time_1
   )
 
   return(wide_tbl)
 }
+
+# approximate relevant parameter values at time_cutoff = 50
+# set.seed(510)
+# pop_sim_tbl <- generate_test_data(n_obs = 100000, null_marginal = TRUE)
+# time_cutoff <- 50
+# pop_params_tbl <- pop_sim_tbl |>
+#   mutate(
+#     trunc_potential_time_0 = if_else(
+#       potential_time_0 > time_cutoff, time_cutoff, potential_time_0
+#     ),
+#     trunc_potential_time_1 = if_else(
+#       potential_time_1 > time_cutoff, time_cutoff, potential_time_1
+#     )
+#   ) |>
+#   summarise(
+#     rmst_0 = mean(trunc_potential_time_0),
+#     rmst_1 = mean(trunc_potential_time_1),
+#     var_0 = var(trunc_potential_time_0),
+#     var_1 = var(trunc_potential_time_1),
+#     rmst_diff = rmst_1 - rmst_0,
+#     diff_var_ratio = var_1 / var_0
+#   )
+
+# recorded values
+# $ rmst_0         <dbl> 37.17233
+# $ rmst_1         <dbl> 37.23047
+# $ var_0          <dbl> 279.903
+# $ var_1          <dbl> 278.9438
+# $ rmst_diff      <dbl> 0.05814
+# $ diff_var_ratio <dbl> 0.9965733
+
+# compute the ratios of truncated survival times
+# diff_var_ratio_vec <- sapply(
+#   seq(from = 0, to = 10),
+#   function(trunc_value) {
+#
+#     # compute the additive diff var ratio under the homogeneous additive
+#     # treatment effect assumption
+#     add_diff_var_ratio <- pop_sim_tbl |>
+#       mutate(
+#         trunc_trunc_potential_time_0 = if_else(
+#           potential_time_0 > (time_cutoff - trunc_value),
+#           (time_cutoff - trunc_value), potential_time_0
+#         ),
+#         trunc_potential_time_1 = if_else(
+#           potential_time_1 > time_cutoff, time_cutoff, potential_time_1
+#         )
+#       ) |>
+#       summarise(
+#         var_0 = var(trunc_trunc_potential_time_0),
+#         var_1 = var(trunc_potential_time_1),
+#         add_diff_var_ratio = var_1 / var_0
+#       ) |>
+#       pull(add_diff_var_ratio)
+#
+#     return(add_diff_var_ratio)
+#   }
+# )
+
+# diff_var_ratio_vec output:
+# 0.9965733 1.0468751 1.1011141 1.1596511 1.2229863 1.2916743 1.3662580 1.4474317
+# 1.5359567 1.6326739 1.7386619
