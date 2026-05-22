cc54200e95effadf987550109141ce9383db4d4f
Philippe Boileau <philippe.boileau@mcgill.ca>
2026-05-22 14:26:41 -0400

    counterfactual outcome generator for TTE setting

--- R/counterfactual-table-generators.R
+++ R/counterfactual-table-generators.R
@@ -1,4 +1,4 @@
-#' Generate Counterfactual Dataset For a Continuous Outcome
+#' Generate Counterfactual Dataset for a Continuous Outcome
 #'
 #' `generate_counterfactural_tbl_fun()` generates a counterfactual version of
 #' `clean_tbl`.
@@ -93,5 +93,119 @@ generate_counterfactural_tbl_fun <- function(
     clean_tbl[[treatment_var_name]]
 
   return(clean_counterfactual_tbl)
+
+}
+
+
+
+#' Generate Counterfactual Dataset for a Time-to-Event Outcome
+#'
+#' `generate_long_counterfactural_tbl_fun()` generates a counterfactual version
+#' of `clean_long_tbl`.
+#'
+#' @inheritParams one_step_var_estimator_fun
+#' @inheritParams generate_counterfactural_tbl_fun
+#' @param clean_long_tbl A pre-processed longitudinal [tibble] that is ready for
+#'   nuisance parameter estimation.
+#' @param cond_event_haz_adj_var_names A `character` vector providing the
+#'   columns names of the adjustment set variables for the conditional event
+#'   hazard estimation stored in `clean_long_tbl`.
+#' @param cond_censoring_haz_adj_var_names A `character` vector providing the
+#'   columns names of the adjustment set variables for the conditional censoring
+#'   hazard estimation stored in `clean_long_tbl`.
+#' @param cond_event_haz_sl_fit A [SuperLearner::SuperLearner] object
+#'   corresponding to the estimated conditional event hazard.
+#' @param cond_censoring_haz_sl_fit A [SuperLearner::SuperLearner] object
+#'   corresponding to the estimated conditional censoring hazard.
+#'
+#' @importFrom rlang :=
+#'
+#' @keywords internal
+#'
+#' @returns A counterfactual longitudinal [tibble], with the additional of
+#'   propensity score, conditional event hazard, conditional censoring hazard,
+#'   conditional event survival, and conditional censoring survival estimates.
+#'
+generate_long_counterfactural_tbl_fun <- function(
+    clean_long_tbl,
+    treatment_group,
+    propensity_score_adj_var_names,
+    cond_event_haz_adj_var_names,
+    cond_censoring_haz_adj_var_names,
+    treatment_var_name,
+    propensity_score_var_name,
+    propensity_score_sl_fit,
+    cond_event_haz_sl_fit,
+    cond_censoring_haz_sl_fit
+) {
+
+  # create counterfactual dataset: all units assigned to treatment_group
+  clean_long_counterfactual_tbl <- clean_long_tbl |>
+    dplyr::mutate(
+      !!treatment_var_name := treatment_group,
+      counterfactual_treatment = treatment_group
+    )
+
+  # predict nuisance parameters under counterfactual scenarios
+
+  ## propensity score predictions (in long format)
+  if (is.null(propensity_score_var_name)) {
+    pred_propensity_score <- SuperLearner::predict.SuperLearner(
+      propensity_score_sl_fit,
+      newdata = clean_long_counterfactual_tbl |>
+        dplyr::select(
+          dplyr::all_of(propensity_score_adj_var_names)
+        ),
+      onlySL = TRUE
+    )$pred
+  } else {
+    pred_propensity_score <- clean_long_tbl[[propensity_score_var_name]]
+  }
+
+  ## conditional event hazards predictions
+  pred_cond_event_haz <- SuperLearner::predict.SuperLearner(
+    cond_event_haz_sl_fit,
+    newdata = clean_long_counterfactual_tbl |>
+      dplyr::select(
+        dplyr::all_of(
+          c("cmldiffvar_long_time", cond_event_haz_adj_var_names,
+            treatment_var_name)
+        )
+      ),
+    onlySL = TRUE
+  )$pred
+
+  ## conditional censoring hazards predictions
+  pred_cond_censoring_haz <- SuperLearner::predict.SuperLearner(
+    cond_censoring_haz_sl_fit,
+    newdata = clean_long_counterfactual_tbl |>
+      dplyr::select(
+        dplyr::all_of(
+          c("cmldiffvar_long_time", cond_censoring_haz_adj_var_names,
+            treatment_var_name)
+        )
+      ),
+    onlySL = TRUE
+  )$pred
+
+  # add predictions to counterfactual dataset and predict conditional event
+  # survival and conditional censoring survival
+  clean_long_counterfactual_tbl <- clean_long_counterfactual_tbl |>
+    dplyr::mutate(
+      pred_propensity_score = as.numeric(pred_propensity_score),
+      pred_cond_event_haz = as.numeric(pred_cond_event_haz),
+      pred_cond_censoring_haz = as.numeric(pred_cond_censoring_haz)
+    ) |>
+    dplyr::group_by(cmldiffvar_id) |>
+    dplyr::mutate(
+      pred_cond_event_survival = cumprod(1 - pred_cond_event_haz),
+      pred_cond_censoring_survival = cumprod(1 - pred_cond_censoring_haz)
+    )
+
+  # add back true treatment assignment vector for record keeping
+  clean_long_counterfactual_tbl[[treatment_var_name]] <-
+    clean_long_tbl[[treatment_var_name]]
+
+  return(clean_long_counterfactual_tbl)
 
 }
--- tests/testthat/test-counterfactual-table-generators.R
+++ tests/testthat/test-counterfactual-table-generators.R
@@ -51,7 +51,7 @@ test_that("counterfactual table generator produces counterfactual tables", {
   # all treatment indicators are set to treatment (1)
   expect(sum(sample_treatment_tbl$treatment), nrow(sample_treatment_tbl))
 
-  # predictions from nuisance parameters match manula calculations
+  # predictions from nuisance parameters match manual calculations
   expect_equal(
     as.numeric(sample_treatment_tbl$pred_propensity_score),
     as.numeric(
@@ -82,3 +82,102 @@ test_that("counterfactual table generator produces counterfactual tables", {
   )
 
 })
+
+test_that(
+  "longitudinal counterfactual table generator produces counterfactual tables",
+  {
+
+    # load required libraries
+    library(dplyr)
+    library(SuperLearner)
+
+    # set seed for reproducibility
+    set.seed(841651)
+
+    # grab a sample of the population and melt their data
+    sample_tbl <- generate_test_data(n_obs = 50)
+    long_sample_tbl <- sample_tbl |>
+      melt_tte_data_fun(
+        baseline_var_names = "w",
+        treatment_var_name = "a",
+        outcome_var_name = "time",
+        censoring_var_name = "censoring",
+        time_cutoff = 50
+      )
+
+    # fit nuisance parameter estimators
+    ps_sl_fit <- estimate_propensity_score_fun(
+      sample_tbl,
+      adj_set_var_names = "w",
+      treatment_var_name = "a",
+      propensity_score_library = c("SL.glm", "SL.mean"),
+      num_nuisance_sl_folds = 5
+    )
+    cond_event_haz_sl_fit <- estimate_cond_event_haz_fun(
+      clean_long_tbl = long_sample_tbl,
+      adj_set_var_names = "w",
+      treatment_var_name = "a",
+      outcome_var_name = "L",
+      cond_event_haz_library = c("SL.glm"),
+      num_nuisance_sl_folds = 5
+    )
+    cond_censoring_haz_sl_fit <- estimate_cond_censoring_haz_fun(
+      clean_long_tbl = long_sample_tbl,
+      adj_set_var_names = "w",
+      treatment_var_name = "a",
+      outcome_var_name = "R",
+      cond_censoring_haz_library = c("SL.glm"),
+      num_nuisance_sl_folds = 5
+    )
+
+    # construct the counterfactual dataset under the treatment group
+    sample_long_treatment_tbl <- generate_long_counterfactural_tbl_fun(
+      clean_long_tbl = long_sample_tbl,
+      treatment_group = 1,
+      propensity_score_adj_var_names = "w",
+      cond_event_haz_adj_var_names = "w",
+      cond_censoring_haz_adj_var_names = "w",
+      treatment_var_name = "a",
+      propensity_score_var_name = NULL,
+      propensity_score_sl_fit = ps_sl_fit,
+      cond_event_haz_sl_fit = cond_event_haz_sl_fit,
+      cond_censoring_haz_sl_fit = cond_censoring_haz_sl_fit
+    )
+
+    # all treatment indicators are set to treatment (1)
+    expect(
+      sum(sample_long_treatment_tbl$counterfactual_treatment),
+      nrow(sample_long_treatment_tbl)
+    )
+
+    # predictions from nuisance parameters match manual calculations
+    expect_equal(
+      as.numeric(sample_long_treatment_tbl$pred_propensity_score),
+      as.numeric(
+        predict(ps_sl_fit, newdata = long_sample_tbl |> select(w))$pred
+      )
+    )
+    expect_equal(
+      as.numeric(sample_long_treatment_tbl$pred_cond_event_haz),
+      as.numeric(
+        predict(
+          cond_event_haz_sl_fit,
+          newdata = long_sample_tbl |>
+            mutate(a = 1) |>
+            select(cmldiffvar_long_time, a, w)
+        )$pred
+      )
+    )
+    expect_equal(
+      as.numeric(sample_long_treatment_tbl$pred_cond_censoring_haz),
+      as.numeric(
+        predict(
+          cond_censoring_haz_sl_fit,
+          newdata = long_sample_tbl |>
+            mutate(a = 1) |>
+            select(cmldiffvar_long_time, a, w)
+        )$pred
+      )
+    )
+  }
+)
